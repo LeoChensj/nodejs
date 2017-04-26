952dfaca6756f5988db00a9e84f54b4912bf7810
MAC <mac@MACs-Air-5.lan>
2017-04-26 09:47:23 +0800

    study

--- 1.js
+++ 1.js
@@ -1,7 +1,6 @@
-//http
-
-
 var http = require('http');
+
+
 http.createServer(function (request, response){
 
 	if(request.uri!=='/favicon.ico'){//清除二次访问
@@ -10,6 +9,6 @@ http.createServer(function (request, response){
 	response.end('Hello World!');
 	}
 
-}).listen(2017);
+}).listen(8080);
 
-console.log('Server running at http://127.0.0.1:2017');
+console.log('Server running');
